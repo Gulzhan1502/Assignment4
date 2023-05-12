66e10468ad765a5e9ac20b0afa25f7f95a832f97
gulzanutekina <gulzhan.utekina@gmail.com>
2023-05-13 01:11:37 +0600

    Testing

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         BST<Integer, String> bst = new BST<>();
+        //test insertion
         bst.insert(50, "A");
         bst.insert(30, "B");
         bst.insert(20, "C");
@@ -10,23 +11,27 @@ public class Main {
         bst.insert(60, "F");
         bst.insert(80, "G");
 
+        // Test in-order traversal
         System.out.println("In-order traversal: ");
         bst.inOrderTraversal();
         System.out.println();
 
+        // Test minimum key
         System.out.println("Minimum key: " + bst.getMin());
+
+        // Test maximum key
         System.out.println("Maximum key: " + bst.getMax());
 
+        // Test get method
+        System.out.println("Value for key 50: " + bst.get(50));
+        System.out.println("Value for key 70: " + bst.get(70));
+        System.out.println("Value for key 90: " + bst.get(90));
+
+        // Test deleting keys
         bst.deleteKey(20);
         bst.deleteKey(30);
         bst.deleteKey(50);
-
-        System.out.println("In-order traversal after deleting keys: ");
+        System.out.println("In-order traversal after deleting keys 20, 30, and 50: ");
         bst.inOrderTraversal();
-
-
-
-
-
     }
 }
--- src/Tree.java
+++
@@ -1,9 +0,0 @@
-public interface Tree <K extends Comparable <K>>{
-    Tree<K> put (K data);
-    void delete(K data);
-    void traverse();
-    K getMax();
-    K getMin ();
-    boolean isEmpty();
-
-}
